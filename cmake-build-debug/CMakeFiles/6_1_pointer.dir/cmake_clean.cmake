4bfda917c6cb8a89bd54b9db6ab808c4e484a438
zhangcy <2304417358@qq.com>
2023-01-14 21:09:56 +0800

    C语言督学营 - 6.1 指针的本质(间接访问原理)

---
+++ cmake-build-debug/CMakeFiles/6_1_pointer.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "6_1_pointer.exe"
+  "6_1_pointer.exe.manifest"
+  "6_1_pointer.pdb"
+  "CMakeFiles/6_1_pointer.dir/main.c.obj"
+  "lib6_1_pointer.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/6_1_pointer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
